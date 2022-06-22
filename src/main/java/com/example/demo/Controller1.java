40916e2d5c03dafedd6279dc9fa5de8da09c45c6
ericmakuan <ericmakuan82502@gmail.com>
2022-06-23 00:04:31 +0800

    thymeleaf 2

--- src/main/java/com/example/demo/Controller1.java
+++ src/main/java/com/example/demo/Controller1.java
@@ -1,14 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.todo.Todo;
+import com.example.demo.todo.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Controller1 {
     @GetMapping("index")
     public String getindex(Model model) {
         model.addAttribute("name","eric");
-        return "index";
+        return "list";
     }
+
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping("/todos")
+    public String getTodos(Model model) {
+        Iterable<Todo> todoList = todoService.getTodos();
+        model.addAttribute("todolist", todoList);
+        Todo todo = new Todo();
+        model.addAttribute("todoObject", todo);
+        return "list";
+    }
+
+
 }
